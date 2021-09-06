cdba4fdd6734a76af5b5c41bc72ea9a08fd26c19
Chris Myburgh <chrismyburgh@wearemonsters.co.za>
2021-09-06 14:39:28 +0200

    FIX - Correct table and field names for SQL updates.

--- update.sql
+++ update.sql
@@ -1,8 +1,8 @@
 #This file contains SQL updates to perform once the wordpress container is running with some customization.
 
-DELETE FROM wp_comments where ID = 1;
+DELETE FROM wp_comments where comment_ID = 1;
 UPDATE wp_terms SET name = 'Success!' where term_id = 1;
-UPDATE wp_post SET comment_status = 'closed' where ID = 1;
-UPDATE wp_post SET post_title = 'Hello Luno!' where ID = 1;
+UPDATE wp_posts SET comment_status = 'closed' where ID = 1;
+UPDATE wp_posts SET post_title = 'Hello Luno!' where ID = 1;
 UPDATE wp_options SET option_value = 'NOT Just another wordpress website.' where option_name = 'blogdescription';
-UPDATE wp_post SET post_content = '<p>This is my automated wordpress deployment using Terraform!</p> <p><img class="" src="https://d540vms5r2s2d.cloudfront.net/mad/uploads/mad_5da8679c5df481571317660.png" width="951" height="602" /></p>' where ID = 1;
+UPDATE wp_posts SET post_content = '<p>This is my automated wordpress deployment using Terraform!</p> <p><img class="" src="https://d540vms5r2s2d.cloudfront.net/mad/uploads/mad_5da8679c5df481571317660.png" width="951" height="602" /></p>' where ID = 1;
